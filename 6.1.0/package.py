8e1544cc4570716a65d6a5d93d3916ebf39ee9a1
David Lai <davidlatwe@gmail.com>
2020-09-21 22:05:07 +0800

    decouple

---
+++ 6.1.0/package.py
@@ -0,0 +1,33 @@
+
+name = "openvdb"
+
+version = "6.1.0"
+
+variants = [
+    ["arch-*", "os-*", "release-1"],
+    ["arch-*", "os-*", "release-0"],
+]
+
+build_requires = [
+    "rezutil-1+",
+    "cmake-3.2+",
+    "boost",
+    "zlib",
+    "blosc",
+    "tbb",
+    "openexr",  # for ilmbase
+]
+build_command = "python {root}/rezbuild.py {install}"
+
+
+def commands():
+    env = globals()["env"]
+
+    env.PATH.append("{root}/bin")
+    env.PATH.append("{root}/lib")
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
+
+    env.OPENVDB_ROOT = "{root}"
+    env.OPENVDB_INCLUDE_DIR.append("{root}/include")
+    env.OPENVDB_LIBRARY.append("{root}/lib")
